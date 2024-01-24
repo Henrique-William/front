a5c80ed01d83c61de7a90a3efb963dd8e6e9050c
Henrique-William <os.henriquewilliam@gmail.com>
2024-01-24 15:53:34 +0000

    Aula 04 - Finalizado (metodos Array)

--- js-metodos-array/app/metodoFilter.js
+++ js-metodos-array/app/metodoFilter.js
@@ -4,6 +4,7 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
-}// essa função consegue capturar o valor dento de um botão e filtrar por cada categoria
+}
+// essa função consegue capturar o valor dento de um botão e filtrar por cada categoria
