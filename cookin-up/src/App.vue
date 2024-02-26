ab954c1fcbb0e4b6bffe21d9d08fcdfba5ada356
Henrique William <os.henriquewiliiam@gmail.com>
2024-02-25 23:03:50 -0300

    initial commit - Alura Cookin-Up

---
+++ cookin-up/src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner }
+}
+
+</script>
+
+<!-- template = HTML do site -->
+<template>
+  <Banner />
+
+  <h1 class="titulo">texto do App.vue</h1>
+</template>
+
+<!-- css do site -->
+<style scoped>
+</style>
+
