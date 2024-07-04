069a5d14def4ad155509c1fc49963a6f32ac2e61
Henrique William <os.henriquewiliiam@gmail.com>
2024-07-04 20:15:07 -0300

    Curso Finalizado - ReactJS (Organo)

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -12,7 +12,8 @@ const Time = (props) => {
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => 
                     <Colaborador 
-                        key={colaborador.nome} 
+                        key={colaborador.nome}
+                        corDeFundo={props.corPrimaria}
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo} 
                         imagem={colaborador.imagem}
