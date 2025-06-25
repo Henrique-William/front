95aeda6fb1c88d4308467e5744f6ad34698f7415
Henrique William <os.henriquewilliam@gmail.com>
2025-06-25 19:54:44 -0300

    Using a Service for Cross-Component Communication

--- investment-calculator-angular/src/app/investment-results/investment-results.component.ts
+++ investment-calculator-angular/src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,7 @@
 import {CurrencyPipe} from '@angular/common';
-import {Component, input} from '@angular/core';
+import {Component, inject} from '@angular/core';
+
+import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-investment-results',
@@ -8,14 +10,11 @@ import {Component, input} from '@angular/core';
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
+
 export class InvestmentResultsComponent {
-  // results = input();
-  results = input<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number
-  }[]>();
+  private investmentService = inject(InvestmentService); //Constructor
+
+  get results() {
+    return this.investmentService.resultData;
+  }
 }
